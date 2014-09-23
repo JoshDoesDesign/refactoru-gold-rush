3e6aaca1f86fc67740e2b264314a1774fe4e1228
Josh Kneeland <josh@joshdoesdesign.com>
2014-09-23 15:50:33 -0600

    Work done up until Tuesday, Sept 23rd, mouse enter and mouseleave states are now working

--- main.js
+++ main.js
@@ -14,15 +14,33 @@ $(document).on('ready', function() {
   		$("textarea").focus();
   	});
 
-  	$(document).on('click', 'button', function() {
-  		console.log("this button works");
-  		var savedValue = $(".note").val();
-		$(this).parent().hide();
-		// $(this).stopPropagation();
-  	});
+    // On click of Submit button....
+    $(document).on('click', 'button', function(e) {
+        var savedValue = $("textarea").val();  // Store the value into a variable
+        $(this).parent().parent().append("<div class = 'note-saved'>" + savedValue + "</div>"); // Append the savedValue after the marker
+        $(this).parent().parent().find(".note-saved").hide();
+        $(this).parent().remove();   // Remove the text box
 
-  	$(document).on('click', '.the-marker', function(e) {
-  		$(this).remove();
- 		e.stopPropagation();
-  	});
+       
+
+        // stop previous click event to keep generating when you click submit
+        e.stopPropagation();
+    });
+
+
+      $(document).on('mouseenter', '.the-marker', function() {
+            $(this).siblings('.note-saved').show();   // Now show the note value
+      });        
+      $(document).on('mouseleave', '.the-marker', function() {
+            // $(this).closest('div').find('div').show();
+            $(this).siblings('.note-saved').hide();   // Now show the savedValue
+      });
+
+    // On click of existing 'the-marker', remove the marker and remove the textbox
+    $(document).on('click', '.the-marker', function(e) {
+        // Remove the parent (the entire div holding everything)
+        $(this).parent().remove();    
+        e.stopPropagation();
+    });
+
 });	
